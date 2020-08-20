581c15548c378cda835ec49c74537e648782ffdd
Julien Cornuwel <julien@cornuwel.net>
2020-08-21 01:24:44 +0200

    Forgot to close brackets

--- manifests/cleanup.pp
+++ manifests/cleanup.pp
@@ -1,7 +1,7 @@
 # @summary This class removes the mainline linux kernel after a successful install
 
 class proxmox::cleanup {
-  package { ['linux-image-amd64','linux-image-4.19*':
+  package { ['linux-image-amd64','linux-image-4.19*']:
     ensure => 'absent'
   }
 ~>exec { 'update-grub': }
