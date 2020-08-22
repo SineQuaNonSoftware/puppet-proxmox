f2287f4b62501d5dd5e0aa5c2afd4d16747b3114
Julien Cornuwel <julien@cornuwel.net>
2020-08-22 22:56:04 +0200

    Try to setup the gateway part of our network config

---
+++ lib/facter/gateway.rb
@@ -0,0 +1,6 @@
+Facter.add("gateway") do
+  setcode do
+    gateway = %x{/bin/grep -E '^[     ]*gateway' /etc/network/
+interfaces|/bin/sed -e 's/^[         ]*gateway[      ]//'}.chomp
+  end
+end
--- manifests/install.pp
+++ manifests/install.pp
@@ -11,15 +11,15 @@ class proxmox::install {
     ensure => 'installed'
   }
   #TODO Configure Postfix properly, maybe as satellites and have a mail server to handle all our sent mail?
-#
-#~>file { '/etc/network/interfaces':
-#    ensure  => 'present',
-#    content => template('proxmox/interfaces.erb'),
-#    owner   => 'root',
-#    group   => 'root',
-#    mode    => '0644'
-#  }
-#  reboot { '/etc/network/interfaces':
-#    subscribe => File['/etc/network/interfaces'],
-#  }
+
+~>file { '/etc/network/interfaces':
+    ensure  => 'present',
+    content => template('proxmox/interfaces.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644'
+  }
+  reboot { '/etc/network/interfaces':
+    subscribe => [ File['/etc/network/interfaces'], Package['proxmox-ve']
+  }
 }
