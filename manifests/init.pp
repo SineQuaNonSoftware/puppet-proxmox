6d04e8cb89ffec58aa0fbb7e4a72406a53702e16
Julien Cornuwel <julien@cornuwel.net>
2020-08-22 23:03:00 +0200

    Tired

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,12 +10,12 @@ class proxmox {
 #  if( $facts['os']['name'] != 'Debian' or $facts['os']['release']['major'] != 10 ) {
 #    fail('This modules only works on Debian 10 "buster"')
 #  }
-  file('/tmp/os':
+  file{ '/tmp/os':
     content => $facts['os']['name']
-  )
-  file('/tmp/ver':
+  }
+  file{ '/tmp/ver':
     content => $facts['os']['release']['major']
-  )
+  }
   contain proxmox::packages # Setup the puppet repo and do another full-upgrade
   contain proxmox::install # Actually install ProxmoxVE and reboots
   contain proxmox::cleanup # Remove mainline linux kernel
