2f9dcd8ba53015fcda7da4b8dd30e219a54b99ee
Julien Cornuwel <julien@cornuwel.net>
2020-08-22 23:40:35 +0200

    Make sure the module is run on a debian 10

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,15 +7,12 @@
 #   include proxmox
 
 class proxmox {
-#  if( $facts['os']['name'] != 'Debian' or $facts['os']['release']['major'] != 10 ) {
-#    fail('This modules only works on Debian 10 "buster"')
-#  }
-  file{ '/tmp/os':
-    content => $facts['os']['name']
+
+  # Make sure the module is run on a debian 10
+  if( $facts['os']['name'] != 'Debian' or $facts['os']['release']['major'] != '10' ) {
+    fail('This modules only works on Debian 10 "buster"')
   }
-  file{ '/tmp/ver':
-    content => $facts['os']['release']['major']
-  }
+
   contain proxmox::packages # Setup the puppet repo and do another full-upgrade
   contain proxmox::install # Actually install ProxmoxVE and reboots
   contain proxmox::cleanup # Remove mainline linux kernel
