8469f147c01218fe42b2f62285e13a037bebaf56
Julien Cornuwel <julien@cornuwel.net>
2020-08-23 20:43:43 +0200

    Woopsie

--- lib/facter/gatewayv4.rb
+++ lib/facter/gatewayv4.rb
@@ -1,5 +1,5 @@
 Facter.add("gatewayv4") do
   setcode do
-    gateway = %x{/bin/grep -E '^[     ]*gateway' /etc/network/interfaces|/bin/sed -e 's/^[         ]*gateway[      ]// | head -1'}.chomp
+    gateway = %x{/bin/grep -E '^[     ]*gateway' /etc/network/interfaces|/bin/sed -e 's/^[         ]*gateway[      ]//' | head -1}.chomp
   end
 end
