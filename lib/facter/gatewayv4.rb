c704f3c9ebd0ff5063358cb92e7404ddc0f5890b
Julien Cornuwel <julien@cornuwel.net>
2020-08-24 01:00:28 +0200

    Perl syntax, anyone?

--- lib/facter/gatewayv4.rb
+++ lib/facter/gatewayv4.rb
@@ -1,5 +1,5 @@
 Facter.add('gatewayv4') do
   setcode do
-    %x{/bin/grep -E '^[     ]*gateway' /etc/network/interfaces|/bin/sed -e 's/^[         ]*gateway[      ]//' | head -1}.chomp
+    `/bin/grep -E '^[     ]*gateway' /etc/network/interfaces|/bin/sed -e 's/^[         ]*gateway[      ]//' | head -1`.chomp
   end
 end
